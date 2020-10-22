51b477114b1839c19f6a261517668b235ec30616
eliasku <deadbabe@gmail.com>
2020-10-22 16:51:20 +0300

    project and test data

---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+#include "main.hpp"
+
+#include <ek/scenex/systems/main_flow.hpp>
+#include <ek/scenex/scene_system.hpp>
+#include <ek/scenex/3d/static_mesh.hpp>
+#include <ek/scenex/components/node.hpp>
+#include <ek/scenex/data/sg_factory.hpp>
+#include <ek/scenex/asset2/builtin_assets.hpp>
+
+namespace ek {
+
+void main() {
+    app::g_app.window_cfg = {"TestFlashApp", {1024, 768}};
+    app::g_app.on_device_ready << [] {
+        run_app<TestFlashApp>();
+    };
+    start_application();
+}
+}
+
+using namespace ek;
+
+TestFlashApp::TestFlashApp()
+        : base_app_type() {
+}
+
+void TestFlashApp::initialize() {
+    base_app_type::initialize();
+    clear_color = float4{0xFF666666_argb};
+}
+
+void TestFlashApp::preload() {
+    base_app_type::preload();
+
+    clear_color_enabled = true;
+}
+
+void TestFlashApp::update_frame(float dt) {
+    base_app_type::update_frame(dt);
+
+    scene_pre_update(root, dt);
+
+    //update_game_title_start(w);
+    scene_post_update(root, dt);
+}
+
+void TestFlashApp::render_frame() {
+//    base_app_type::render_frame();
+    if (!started_) return;
+
+    draw_node(root);
+}
+
+void TestFlashApp::start_game() {
+//    append(game, sg_create("patch9", "nine_patch_test"));
+    append(game, sg_create("tests", "test"));
+//    append(game, sg_create("test_0", "test"));
+//    append(game, sg_create("improvements", "scene2"));
+}
+
+TestFlashApp::~TestFlashApp() = default;
---
+++ src/main.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#ifdef EK_EDITOR
+
+#include <ek/editor/editor_app.hpp>
+
+using base_app_type = ::ek::editor_app_t;
+
+#else
+
+#include <ek/scenex/app/basic_application.hpp>
+
+using base_app_type = ::ek::basic_application;
+
+#endif
+
+class TestFlashApp : public base_app_type {
+public:
+    TestFlashApp();
+
+    ~TestFlashApp() override;
+
+    void initialize() override;
+
+    void preload() override;
+
+protected:
+    void update_frame(float dt) override;
+
+    void render_frame() override;
+
+    void start_game() override;
+};
+
